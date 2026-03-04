5ca60b79e92d293f56f7db83cfa44321e9b27fab
Jorge Peris <jorgeperis@gmail.com>
2026-03-04 10:57:46 +0100

    IMprove deployment

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Arrow
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    config.active_storage.variant_processor = :disabled
   end
 end
